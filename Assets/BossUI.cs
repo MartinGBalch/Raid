7868ba306a51b65db45bff57f94c69d6ad673a23
MartinGBalch <martybalch21@live.com>
2017-05-21 07:23:03 -0700

    Added player and boss health UI

---
+++ Assets/BossUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class BossUI : MonoBehaviour {
+
+
+    Text DUI;
+    BossHealth health;
+    public GameObject Boss;
+    // Use this for initialization
+    void Start()
+    {
+        health = Boss.GetComponent<BossHealth>();
+        DUI = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DUI.text = health.Health.ToString();
+
+        if(health.ResistDamage == 0)
+        {
+            DUI.enabled = true;
+        }
+        else
+        {
+            DUI.enabled = false;
+        }
+    }
+}
---
+++ Assets/UI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UI : MonoBehaviour {
+
+    Text DUI;
+    PlayerHealth health;
+    public GameObject Player;
+	// Use this for initialization
+	void Start ()
+    {
+        health = Player.GetComponent<PlayerHealth>();
+        DUI = GetComponent<Text>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        DUI.text = health.Health.ToString();
+
+	}
+}
