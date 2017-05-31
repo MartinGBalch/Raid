90a4f6ae8fe3352f65f780735483cfef92ebcb32
Martin Balch <martin.balch@ad.aie.edu>
2017-05-31 11:14:26 -0700

    controller support for menus

--- Assets/Martin's Scripts/ControlScreenManager.cs
+++ Assets/Martin's Scripts/ControlScreenManager.cs
@@ -1,16 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class ControlScreenManager : MonoBehaviour {
+
+public class ControlScreenManager : MonoBehaviour
+{
+    public Image XboxControls;
+    public Image KeyboardControls;
+    public Image PS4Controls;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        XboxControls.enabled = true;
+        KeyboardControls.enabled = false;
+        PS4Controls.enabled = false;
+        Cursor.visible = true;	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update () {}
+
+    public void LoadXboxControls()
+    {
+        XboxControls.enabled = true;
+        KeyboardControls.enabled = false;
+        PS4Controls.enabled = false;
+    }
+
+    public void LoadKeyboardControls()
+    {
+        XboxControls.enabled = false;
+        KeyboardControls.enabled = true;
+        PS4Controls.enabled = false;
+    }
+
+    public void LoadPS4Controls()
+    {
+        XboxControls.enabled = false;
+        KeyboardControls.enabled = false;
+        PS4Controls.enabled = true;
+    }
 }
--- Assets/Martin's Scripts/PauseMenu.cs
+++ Assets/Martin's Scripts/PauseMenu.cs
@@ -7,10 +7,12 @@ using UnityEngine.UI;
 public class PauseMenu : MonoBehaviour
 {
     public Canvas PauseCanvas;
-    public Canvas InGameSettings;
-    public Canvas ControlsCanvas;
+    //public Canvas InGameSettings;
+    //public Canvas ControlsCanvas;
 
-   
+    //public Image XboxControls;
+    //public Image KeyboardControls;
+    //public Image PS4Controls;
 
     public bool paused;
 
@@ -19,10 +21,11 @@ public class PauseMenu : MonoBehaviour
     // Use this for initialization
     public void Start()
     {
+        
         paused = false;
         PauseCanvas.enabled = false;
         //InGameSettings.enabled = false;
-        ControlsCanvas.enabled = false;
+        //ControlsCanvas.enabled = false;
         //ControlsCanvas.GetComponent<Canvas>().enabled = false;
         //ControlsCanvas.
     }
@@ -59,22 +62,43 @@ public class PauseMenu : MonoBehaviour
         SceneManager.LoadScene("Menu");
     }
 
-    public void Settings()
-    {
-        InGameSettings.enabled = true;
-    }
+    //public void Settings()
+    //{
+    //    InGameSettings.enabled = true;
+    //}
 
-    public void Controls()
-    {
-        ControlsCanvas.enabled = true;
-       
-    }
+    //public void Controls()
+    //{
+    //    ControlsCanvas.enabled = true;
+    //}
 
-    public void Back()
-    {
-        PauseCanvas.enabled = true;
-        InGameSettings.enabled = false;
-        InGameSettings.enabled = false;
+    //public void Back()
+    //{
+    //    PauseCanvas.enabled = true;
+    //    InGameSettings.enabled = false;
+    //    ControlsCanvas.enabled = false;
+        
        
-    }
+    //}
+
+    //public void LoadXboxControls()
+    //{
+    //    XboxControls.enabled = true;
+    //    KeyboardControls.enabled = false;
+    //    PS4Controls.enabled = false;
+    //}
+
+    //public void LoadKeyboardControls()
+    //{
+    //    XboxControls.enabled = false;
+    //    KeyboardControls.enabled = true;
+    //    PS4Controls.enabled = false;
+    //}
+
+    //public void LoadPS4Controls()
+    //{
+    //    XboxControls.enabled = false;
+    //    KeyboardControls.enabled = false;
+    //    PS4Controls.enabled = true;
+    //}
 }
