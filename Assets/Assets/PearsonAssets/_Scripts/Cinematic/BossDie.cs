507b232c4b3c6cc4d34ba4209c0f26e3954e1053
Pearson Lawrence <pearson.lawrenc@ad.aie.edu>
2017-06-07 09:22:32 -0700

    fixes

---
+++ Assets/Assets/PearsonAssets/_Scripts/Cinematic/BossDie.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossDie : MonoBehaviour {
+
+    private BossHealth BossHP;
+    private Animator PlayerAnim;
+    public float obsorbTime;
+	// Use this for initialization
+	void Start () {
+        BossHP = FindObjectOfType<BossHealth>();
+        PlayerAnim = GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        obsorbTime -= Time.deltaTime;
+	}
+}
