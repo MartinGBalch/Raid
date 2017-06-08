48b9c0875d47820abb18098b8a39b7e9241868c6
kobey.myhre <kobey.myhre@students.aie.edu.au>
2017-06-08 15:10:23 -0700

    Rock Smash and object pooling

--- Assets/Boss Stuff/Boss-Scripts/RockDamage.cs
+++ Assets/Boss Stuff/Boss-Scripts/RockDamage.cs
@@ -4,27 +4,63 @@ using UnityEngine;
 
 public class RockDamage : MonoBehaviour {
 
+    private TimeManager DeltaTime;
     public float Damage;
     public float TimeToKill;
-
-    void OnCollisionEnter(Collision other)
+    Vector3 UpPos;
+    Vector3 ForPos;
+    public float UpSpeed;
+    public float ForSpeed;
+    public float UpDistance;
+    public float ForDistance;
+    float DT;
+    Transform MyT;
+    public float HeightUnderFloor;
+    void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Player")
+        if(other.tag == "Player")
         {
-            var player = other.gameObject.GetComponent<PlayerHealth>();
+            var player = other.GetComponent<PlayerHealth>();
             player.TakeDamage(Damage);
         }
+        if (other.tag == "Pylon") { Destroy(gameObject); }
+        if (other.tag == "Pillar") { Destroy(gameObject); }
     }
 
 
 	// Use this for initialization
 	void Start ()
     {
+        MyT = GetComponent<Transform>();
+        DeltaTime = FindObjectOfType<TimeManager>();
+        //UpPos = MyT.position + (MyT.up*UpDistance);
+        ForPos = MyT.position + (MyT.forward * ForDistance);
+
         Destroy(gameObject, TimeToKill);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+        RaycastHit hit;
+        if (Physics.Raycast(MyT.position, Vector3.up, out hit, 50))
+        {
+            if (hit.collider.tag == "Floor")
+            {
+                float distDIF = Mathf.Abs(hit.point.y - MyT.position.y);
+
+                Vector3 NewPos = new Vector3(MyT.position.x, hit.point.y, MyT.position.z);
+                UpPos = NewPos;
+                NewPos.y -= HeightUnderFloor;
+                MyT.position = NewPos;
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        Debug.DrawLine(MyT.position, UpPos);
+        Debug.DrawLine(MyT.position, ForPos);
+        DT = DeltaTime.DT;
+       // MyT.position = Vector3.Lerp(MyT.position, UpPos, UpSpeed * DT);
+        MyT.position = Vector3.Lerp(MyT.position, ForPos, ForSpeed * DT);
+
 	}
 }
--- Assets/Boss Stuff/Boss-Scripts/SpawnBoulders.cs
+++ Assets/Boss Stuff/Boss-Scripts/SpawnBoulders.cs
@@ -12,25 +12,44 @@ public class SpawnBoulders : MonoBehaviour {
     public float Range;
     public float heightMax;
     public float heightMin;
+    List<GameObject> ListOFun;
+    public int PooledObjectCount;
 
     public void RunMechanic()
     {
        
             for (int i = 0; i < spawnCount; i++)
             {
-                float RandX = Random.Range(-Range, Range);
-                float RandZ = Random.Range(-Range, Range);
-                float RandY = Random.Range(heightMin, heightMax);
-                var AoE = AoeEffect;
-                AoE.transform.position = new Vector3(transform.position.x + RandX, RandY, transform.position.z + RandZ);
-                Instantiate(AoE);
+                for(int j = 0; j < ListOFun.Count; j++)
+            {
+                if(!ListOFun[j].activeInHierarchy)
+                {
+                    float RandX = Random.Range(-Range, Range);
+                    float RandZ = Random.Range(-Range, Range);
+                    float RandY = Random.Range(heightMin, heightMax);
+                    ListOFun[j].SetActive(true);
+                    ListOFun[j].transform.position = new Vector3(transform.position.x + RandX, RandY, transform.position.z + RandZ);
+                    break;
+                    //var AoE = AoeEffect;
+                    //AoE.transform.position = new Vector3(transform.position.x + RandX, RandY, transform.position.z + RandZ);
+                    //Instantiate(AoE);
+                }
+            }
+                
             }
         
     }
     // Use this for initialization
     void Start ()
     {
-       
+        ListOFun = new List<GameObject>();
+        for (int i = 0; i < PooledObjectCount; i++)
+        {
+            GameObject baby = (GameObject)Instantiate(AoeEffect);
+            baby.SetActive(false);
+
+            ListOFun.Add(baby);
+        }
     }
 	
 	// Update is called once per frame
