608703605e2dc47506cdf8af43628468a73a7272
PearsonLawrence <pearson.lawrence@students.aie.edu.au>
2017-06-10 11:59:16 -0700

    level design and organization

---
+++ Assets/Assets/PearsonAssets/_Scripts/Cinematic/FloorController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorController : MonoBehaviour {
+    private BossStateManagerTwo BossState;
+    bool fall;
+    public float falldelay;
+    public float fallspeed;
+    float falltime;
+    public GameObject platforms;
+    private TimeManager DT;
+	// Use this for initialization
+	void Start () {
+        BossState = FindObjectOfType<BossStateManagerTwo>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(BossState.State == 3)
+        {
+            if(fall == false)
+            {
+                falltime = falldelay;
+                fall = false;
+            }
+            falltime -= DT.DT;
+            if(falltime <= 0)
+            {
+                transform.position -= new Vector3(0, DT.DT * fallspeed, 0);
+
+                platforms.transform.position = new Vector3(platforms.transform.position.x, Mathf.Clamp(platforms.transform.position.y, -12.2f, -10.5f), 0);
+            }
+            
+        }
+		
+	}
+}
